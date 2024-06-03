f833a904acb7381458329638550daa25e52331a5
Aadisheshudupa <aadiudu@gmail.com>
2024-06-03 16:03:48 +0530

    byee

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,16 @@ import { OrbitControls } from '@react-three/drei'
 import { useLoader } from '@react-three/fiber'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
 import  {Stats} from '@react-three/drei'
+
+import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader'
+
 export default function App() {
-  const gltf = useLoader(GLTFLoader, '/tired2.glb');
+  const gltf = useLoader(GLTFLoader, '/tired2.glb', (loader) => {
+    const dracoLoader = new DRACOLoader()
+    dracoLoader.setDecoderPath('https://www.gstatic.com/draco/versioned/decoders/1.5.7/')
+    loader.setDRACOLoader(dracoLoader)
+})
+
   return (
     <>
       <Canvas camera={{position: [-8, 5, 8]}}>
